8281d5620d5db87573ff0109b8b03f9f32efdfb3
anshul <anshulprajapati990@gmail.com>
2024-03-11 15:57:18 +0530

    initial commit

---
+++ src/main/java/com/anshul/besocial/BeSocialApplication.java
@@ -0,0 +1,13 @@
+package com.anshul.besocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anshul/besocial/BeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshul.besocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
